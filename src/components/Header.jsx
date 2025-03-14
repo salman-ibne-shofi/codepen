ed55216a8b924cb18ef4803ff55c56f897477a02
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-03-15 01:52:16 +0600

    changed the background of the header and added a codepen site logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import { AppBar, Toolbar, styled } from "@mui/material";
+
+const Container = styled(AppBar)`
+	background: #060606;
+	height: 9vh;
+`;
 
 const Header = () => {
-    return (
-        <div>
-            Hello0 from header
-        </div>
-    );
+	const logo = "https://i.ibb.co.com/Ndt5nFqf/codepen-icon-26.png";
+
+	return (
+		<Container>
+			<Toolbar>
+				<img src={logo} alt="logo" style={{ width: 40 }} />
+			</Toolbar>
+		</Container>
+	);
 };
 
 export default Header;
