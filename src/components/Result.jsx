9e181655ef4fbf6f53022c4c3919e3b64b74fbb1
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-03-17 02:39:42 +0600

    updated the result dot jsx file

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,29 +1,46 @@
-import { useContext } from "react";
-import { Box } from "@mui/material";
+import { useContext, useState, useEffect } from "react";
+import { Box, styled } from "@mui/material";
 import { DataContext } from "../context/DataProvider";
 
+const Container = styled(Box)`
+	height: 41vh;
+`;
+
 const Result = () => {
+	const [src, setSrc] = useState("");
 	const { html, css, js } = useContext(DataContext);
 
-	const srcCode = `
+	useEffect(() => {
+		const srcCode = `
         <html>
-            <body>${html}</body>
-            <style>${css}</style>
-            <script>${js}</script>
+            <head>
+                <style>${css || ""}</style>
+            </head>
+            <body>
+                ${html || ""}
+                <script>${js || ""}</script>
+            </body>
         </html>
-    `;
+        `;
+
+		const timeout = setTimeout(() => {
+			setSrc(srcCode);
+		}, 1000);
+
+		return () => clearTimeout(timeout);
+	}, [html, css, js]);
 
 	return (
-		<Box>
+		<Container>
 			<iframe
-				srcDoc={srcCode}
+				srcDoc={src}
 				title="Output"
 				sandbox="allow-scripts allow-same-origin"
 				frameBorder={0}
 				width="100%"
 				height="100%"
 			/>
-		</Box>
+		</Container>
 	);
 };
 
